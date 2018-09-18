6e9fb0349c928667638a222406afd4cb8f82d84d
fqtgit <15915778735@163.com>
2018-09-18 18:36:08 +0800

    none

--- src/main/java/com/fqt/project/controller/RoleController.java
+++ src/main/java/com/fqt/project/controller/RoleController.java
@@ -1,16 +1,24 @@
 package com.fqt.project.controller;
 
+import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+
+import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.fqt.project.constant.JxcConstant;
+import com.fqt.project.entity.Log;
 import com.fqt.project.entity.Role;
+import com.fqt.project.entity.User;
+import com.fqt.project.service.LogService;
 import com.fqt.project.service.RoleService;
+import com.fqt.project.util.DateUtil;
 
 /**
  * 角色控制层
@@ -23,19 +31,24 @@ public class RoleController {
 	
 	@Autowired
 	private RoleService roleService;
+	@Autowired
+	private LogService logService;
 
 	@RequestMapping("/getAllRoleInfos")
 	@ResponseBody
-	public Map<String,Object> getAllRoleInfos(Integer page,Integer rows){
+	public Map<String,Object> getAllRoleInfos(Integer page,Integer rows,HttpSession session){
 		Map<String,Object> map=new HashMap<String,Object>();
 		try {
 			long totalItem=roleService.getAllRoleCount();
 			List<Role> roleList=roleService.getAllRoleInfosByPage((page-1)*rows,rows*page);
 			map.put("rows", roleList);
 			map.put("total", totalItem);
+			User currentUser=(User) session.getAttribute("currentUser");
+			String datetime=DateUtil.DatetoString(new Date());
+			Log log=new Log(currentUser.getUserName()+"查询角色", datetime, JxcConstant.SEARCH_ACTION, currentUser);
+			logService.insertLog(log);
 		} catch (Exception e) {
 			// TODO: handle exception
-			e.printStackTrace();
 		}
 		return map;
 	}
